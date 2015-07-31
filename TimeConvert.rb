026a9bdc98727a15792eda3fa2dcbcb6bffa2243
HJPark <hyeonjoopark88@gmail.com>
2015-07-31 12:49:50 +0900

    Update TimeConvert.rb

--- TimeConvert.rb
+++ TimeConvert.rb
@@ -1,12 +1,10 @@
 def TimeConvert(num)
 
   # code goes here
-  hours = (num.to_i)/60
-  minutes = (num.to_i)%60
-  return "#{hours}:#{minutes}"
+  return (num/60).to_s+":"+(num%60).to_s
          
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-TimeConvert(STDIN.gets)    
+TimeConvert(STDIN.gets)        
