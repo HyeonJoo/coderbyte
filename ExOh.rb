2bdcdd719914b12a96c46684976af669709762f7
HJPark <hyeonjoopark88@gmail.com>
2015-07-31 16:17:22 +0900

    Update and rename ExOh to ExOh.rb

--- ExOh.rb
+++ ExOh.rb
@@ -3,10 +3,9 @@ def ExOh(str)
   # code goes here
   if (str.count "x") == (str.count "o")
     return true
-  else
-    return false
   end
-         
+  
+  return false
 end
    
 # keep this function call here 
